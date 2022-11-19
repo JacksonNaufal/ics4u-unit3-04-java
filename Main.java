9e5fdc45ff28302817fc68fbd23acd99913f4a57
Jackson-Naufal <Jackson.naufal@mths.ca>
2022-11-19 17:43:29 +0000

    fixes

--- Main.java
+++ Main.java
@@ -1,15 +1,14 @@
 /*
-* This is a standard reverse string program
-* that reverses the string inputed.
+* This is a towerOfHanoi program.
+* That rearrages everthing for the
+* program.
 *
 * @author  Jackson Naufal
 * @version 1.0
-* @since   2020-11-12
+* @since   2020-11-18
 *
 * This is a string reverse program
 */
-import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -17,26 +16,6 @@ import java.util.Scanner;
  * Main Class.
  */
 final class Main {
-
-    /**
-    * The min number for array.
-    */
-    public static final int ARRAY_SIZE = 250;
-
-    /**
-     * This input is invalid.
-     */
-    public static final String INVALID = "ERROR: Invalid Input";
-
-    /**
-    * The max number for array.
-    */
-    public static final int MAX = 999;
-
-    /**
-    * The number of elements in the array.
-    */
-    public static final int MIN = 0;
 
     /**
     * Prevent instantiation.
@@ -52,94 +31,53 @@ final class Main {
     }
 
     /**
-    * Function finds the index of a number, using Binary Search recursively.
-    *
-    * @param userArray the userArray.
-    * @param userNumber the userNumber.
-    * @param lowIndex the lowIndex.
-    * @param highIndex the highIndex.
-    * @return binarySearch the binarySearch.
-    */
-    static int binarySearch(final int[] userArray, final int userNumber,
-                          final int lowIndex, final int highIndex) {
-        // https://www.geeksforgeeks.org/binary-search/
-        // Binary search in python, converted to java
-        final int returns;
-        if (lowIndex > highIndex) {
-            returns = -1;
-
+     * This is the hanoi function.
+     *
+     * @param nOfDisks this is the number of disks.
+     * @param startPeg this is the start peg.
+     * @param endPeg this is the end peg.
+     */
+    static void hanoi(final int nOfDisks, final int startPeg,
+                             final int endPeg) {
+        // This function calculates where the disks should be placed in
+        final int pegNumber = 6;
+        if (nOfDisks == 1) {
+            System.out.println("Move disk 1 from peg " + startPeg + " to peg "
+                               + endPeg);
         } else {
-            final int mid = (lowIndex + highIndex) / 2;
-            if (userNumber == userArray[mid]) {
-                returns = mid;
-
-            } else if (userNumber > userArray[mid]) {
-                returns = binarySearch(userArray, userNumber, mid
-                                + 1, highIndex);
-            } else {
-                returns = binarySearch(userArray, userNumber,
-                                lowIndex, mid - 1);
-            }
+            hanoi(nOfDisks - 1, startPeg, pegNumber - startPeg - endPeg);
+            System.out.println("Move disk " + nOfDisks + " from the peg "
+                            + startPeg + " to peg" + endPeg);
+            hanoi(nOfDisks - 1, pegNumber - startPeg - endPeg, endPeg);
         }
-        return returns;
     }
 
     /**
-    * This starting main() function.
-    *
-    * @param args No args will be used
-    */
+     * This starting main() function.
+     *
+     * @param args No args will be used
+     */
     public static void main(final String[] args) {
-        System.out.println("Binary Search Program");
+        final int startPeg = 1;
+        final int endPeg = 3;
+
+        System.out.println("Tower of Hanoi");
+
+        // input
+        final Scanner userInput = new Scanner(System.in);
+        System.out.print("\nHow many disks do you want?: ");
+
         try {
-            // Initializing the random class
-            final Random randNumber = new Random();
-
-            // Initializing array of numbers
-            final int[] randomNumberArray = new int[ARRAY_SIZE];
-
-            // Adding numbers to the array
-            for (int counter = 0; counter
-                            < randomNumberArray.length; counter++) {
-                randomNumberArray[counter] = randNumber.nextInt(MAX) + 1;
-            }
-
-            // Sorting the array
-            final int[] numberArray = randomNumberArray;
-            Arrays.sort(numberArray);
-
-            System.out.print("\nSorted list of numbers:\n");
-            for (int element: numberArray) {
-                final String padded = String.format("%03d", element);
-                System.out.print(padded + ", ");
-            }
-            System.out.print("\n\n");
-
-            // Getting user input as to what number they wish to search for
-            final Scanner userInput = new Scanner(System.in);
-            System.out.print("What number are you searching for in the array");
-            System.out.print(" (integer between 0 and 999): ");
-            final int searchNumber = userInput.nextInt();
-            userInput.close();
+            final int nOfDisks = userInput.nextInt();
             System.out.println();
-
-            // Ensuring the user inputs an appropriate integer
-            if (searchNumber > MAX || searchNumber < MIN) {
-                System.out.println(INVALID);
+            if (nOfDisks > 0) {
+                // process
+                hanoi(nOfDisks, startPeg, endPeg);
             } else {
-                // Using binary search to find the user's
-                // chosen number in the array
-                final int searchResult = binarySearch(numberArray,
-                                searchNumber, 0, numberArray.length - 1);
-                // Outputing the results of the search
-                System.out.println();
-                System.out.println("Your number is in index: " + searchResult);
+                System.out.println("\nPlease enter a positive integer");
             }
-
-        // Catches and tells the user that an error occured
         } catch (java.util.InputMismatchException ex) {
-            System.out.println();
-            System.out.println(INVALID);
+            System.err.print("\nThis is not an integer");
         }
     }
 }
